5e465a7b1a7765d21a178aee6db92c5396a67487
meet1075 <meetsoni1075@gmail.com>
2025-02-05 20:06:25 +0530

    database setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,19 @@
+// import mongoose from "mongoose"
+import 'dotenv/config';
+
+import connectDB from "./db/index.js";
+import {app} from "./app.js";
+
+
+
+connectDB()
+.then(()=>{                      
+        app.listen(process.env.PORT||8000,()=>{
+            console.log(`server running at port : ${process.env.PORT}`)
+        })
+    }
+)
+.catch((err)=>{
+    console.log("MONGODB connection failed",err)
+})
 
