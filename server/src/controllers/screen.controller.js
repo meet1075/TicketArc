cfa62e65cf129033189e73c93357b5947496fa66
tirth2404 <sorathia24ts@gmail.com>
2025-02-27 12:23:49 +0530

    checking clone

--- server/src/controllers/screen.controller.js
+++ server/src/controllers/screen.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiErrors } from "../utils/ApiErrors.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import {Theater} from "../models/theater.model.js"
+import {Theater} from "../models/theater.model.js" 
 import { Screen } from "../models/screen.model.js";
 import mongoose, { isValidObjectId } from 'mongoose';
 const updateScreen= asyncHandler(async(req,res)=>{
